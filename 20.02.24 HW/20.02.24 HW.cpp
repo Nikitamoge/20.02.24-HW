9922ca06f7cd351631f4a6cc5fd4857d3b0e8da6
Nikita Iliuschchenko <Nike.il.od@gmail.com>
2024-04-03 16:46:57 +0200

    Add project files.

---
+++ 20.02.24 HW/20.02.24 HW.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string Compress(const string& text) {
+    string result;
+    int spaceCount = 0;
+
+    for (char ch : text) {
+        if (ch != ' ') {
+            if (spaceCount > 0) {
+                result += ' ';
+                spaceCount = 0;
+            }
+            result += ch;
+        }
+        else {
+            if (result.empty() || result.back() == ' ') {
+                result += ch;
+            }
+            else {
+                spaceCount++;
+            }
+        }
+    }
+    return result;
+}
+
+int main() {
+    string text = "Hello         what's     your name   and     age?";
+    string compressed = Compress(text);
+    cout << compressed << endl;
+}
